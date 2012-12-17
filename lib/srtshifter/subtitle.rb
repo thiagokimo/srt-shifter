1d3b9ac101d8aaccca3bcedc89e063685fb411e7
Kimo <kimobr@gmail.com>
2012-12-17 14:27:10 -0200

    Add time regex

--- lib/srtshifter/subtitle.rb
+++ lib/srtshifter/subtitle.rb
@@ -9,9 +9,31 @@ module SrtShifter
 
 		def shift
 
+			output_file = File.open(@opts[:output], 'w')
+
 			lines = IO.readlines(@opts[:input])
 
-			File.open(@opts[:output], 'w') {|f| f.write("test") }
+
+			lines.each do |line|
+
+				time_match = line.scan(/(\d{2}:\d{2}:\d{2},\d{3})/)
+
+				# if time_line.size > 0
+				# 	output_file.write("MUDOU\n")
+				# else
+				# 	output_file.write(line)
+				# end
+						
+			end
+
+			# #time_lines = lines.scan(/(\d{2}:\d{2}:\d{2}\D\d{3}) --> (\d{2}:\d{2}:\d{2}\D\d{3})/)
+			# time_lines = lines.scan(/\d/)
+
+			# puts time_lines
+
+			# #puts time_lines
+
+			#File.open(@opts[:output], 'w') {|f| f.write("test") }
 
 		end
 
