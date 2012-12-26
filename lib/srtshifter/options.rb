99124e8267fce82e1cdad3dfc0d270a8c789fcc9
Kimo <kimobr@gmail.com>
2012-12-26 13:25:49 -0200

    Lots of changes in Options class
    It was encapsuled in an object and now it is being instantiated inside Shifter class

--- lib/srtshifter/options.rb
+++ lib/srtshifter/options.rb
@@ -3,49 +3,63 @@ require 'optparse'
 module SrtShifter
   class Options
 
-    operations = ["ADD", "SUB"]
+    attr_accessor :options
 
-    options = {}
-    
-    ARGV.options do |opts|
+    def initialize(args)
+      @arguments = args
 
-      opts.banner = "Usage:  #{File.basename($PROGRAM_NAME)} [OPTIONS] input_file output_file"
-      
-      opts.separator ""
-      opts.separator "Common Options:"
+      @operations = ["ADD", "SUB"]
 
-      opts.on("-o","--operation (ADD|SUB)", String, "Type ADD to increase time or SUB to decrease time.") do |o|
-        options[:operation] = o.upcase if operations.include? o.upcase
+      @options = {}
+    end
+
+    def parse_options
+      @arguments.options do |opts|
+
+        opts.banner = "Usage:  #{File.basename($PROGRAM_NAME)} [OPTIONS] input_file output_file"
+        
+        opts.separator ""
+        opts.separator "Common Options:"
+
+        opts.on("-o","--operation (ADD|SUB)", String, "Type ADD to increase time or SUB to decrease time.") do |o|
+          @options[:operation] = o
+        end
+
+        opts.on("-t","--time (VALUE)", Float, "Time in milliseconds.") do |n|
+          @options[:time] = n
+        end
+
+        opts.on_tail('-h', '--help', 'Show this help message.') do
+          puts(opts)
+          exit(0)
+        end
+        
+        opts.parse!
       end
+    end
 
-      opts.on("-t","--time (VALUE)", Float, "Time in milliseconds.") do |n|
-        options[:time] = n
-      end
-
-      opts.on_tail('-h', '--help', 'Show this help message.') do
-        puts(opts)
-        exit(0)
-      end
-      
-      opts.parse!
-
+    def validate_args
       begin
-        raise "Operation option is missing" if options[:operation].nil?
-        raise "Time option is missing" if options[:time].nil?
-        
-        raise "Input file is missing" if ARGV[0].nil?
-        options[:input] = ARGV[0]
-        
-        raise "Output file is missing" if ARGV[1].nil?
-        options[:output] = ARGV[1]
 
-        subtitle = SrtShifter::Subtitle.new(options)
-        subtitle.shift
 
-      rescue Exception => ex
-        puts "#{ex.message}. Please use -h or --help for usage."
-        exit(1)
-      end
+          raise "Operation option is missing" if @options[:operation].nil?
+          raise "Unknown operation: #{@options[:operation]}" unless @operations.include? @options[:operation].upcase
+
+          raise "Time option is missing" if @options[:time].nil?
+                    
+          raise "Input file is missing" if @arguments[0].nil?
+          @options[:input] = @arguments[0]
+          
+          raise "Output file is missing" if @arguments[1].nil?
+          @options[:output] = @arguments[1]
+
+          # subtitle = SrtShifter::Subtitle.new(@options)
+          # subtitle.shift
+
+        rescue Exception => ex
+          puts "#{ex.message}. Please use -h or --help for usage."
+          exit(1)
+        end
     end
   end
 end
