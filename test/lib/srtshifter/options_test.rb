7e8257f000326f42ebe2a958bc3806c66ba2f7cc
Thiago Rocha <kimobr@gmail.com>
2013-04-17 10:55:08 -0300

    A little Refactor in tests

--- test/lib/srtshifter/options_test.rb
+++ test/lib/srtshifter/options_test.rb
@@ -1,56 +1,49 @@
 require "test_helper"
 
-describe SrtShifter do
-
-	describe "Options" do
+describe SrtShifter::Options do
 		
-		describe "validate_args" do
+	describe "validate_args" do
 
-			before(:each) do
-				@args = {:operation => "ADD", :time => 10.0, :input => "test/samples/test_srt.srt", :output => "test_output.srt"}
-			end
-
-			after(:each) do
-				@args = nil
-			end
-			
-			it "raise error when get none operation option" do
-				
-				args = ""
-				opts = SrtShifter::Options.new args
-
-				lambda { opts.validate_args }.must_output("Operation option is missing. Please use -h or --help for usage.\n", nil)
-
-			end
-
-			it "raise error when operation is unknown" do
-				
-				opts = SrtShifter::Options.new ""
-				opts.options[:operation] = "bla"
-
-				lambda { opts.validate_args }.must_output("Unknown operation: bla. Please use -h or --help for usage.\n", nil)				
-			end
-
-			it "must accept lowercase existing operations" do
-
-				opts = SrtShifter::Options.new "test/samples/test_srt.srt test_output.srt"
-				opts.options = @args
-				opts.options[:operation] = "sub"
-
-
-				lambda { opts.validate_args }.must_be_silent				
-			end
-
-			it "raise error when get none time option"
-
-			it "raise error when get none time value"
-
-			it "raise error when get none input file path" 
-
-			it "raise error when get none output file path"
-
+		before(:each) do
+			@args = {:operation => "ADD", :time => 10.0, :input => "test/samples/test_srt.srt", :output => "test_output.srt"}
 		end
 
-	end
+		after(:each) do
+			@args = nil
+		end
+		
+		it "raise error when get none operation option" do
+			
+			args = ""
+			opts = SrtShifter::Options.new args
 
+			lambda { opts.validate_args }.must_output("Operation option is missing. Please use -h or --help for usage.\n", nil)
+		end
+
+		it "raise error when operation is unknown" do
+			
+			opts = SrtShifter::Options.new ""
+			opts.options[:operation] = "bla"
+
+			lambda { opts.validate_args }.must_output("Unknown operation: bla. Please use -h or --help for usage.\n", nil)				
+		end
+
+		it "must accept lowercase existing operations" do
+
+			opts = SrtShifter::Options.new "test/samples/test_srt.srt test_output.srt"
+			opts.options = @args
+			opts.options[:operation] = "sub"
+
+			lambda { opts.validate_args }.must_be_silent				
+		end
+
+		it "raise error when get none time option"
+
+		it "raise error when get none time value"
+
+		it "raise error when get none input file path" 
+
+		it "raise error when get none output file path"
+
+	end
 end
--- test/lib/srtshifter/subtitle_test.rb
+++ test/lib/srtshifter/subtitle_test.rb
@@ -1,88 +1,84 @@
 require "test_helper"
 
-describe SrtShifter do
-
-	describe "SUBTITLE" do
+describe SrtShifter::Subtitle do
 		
-		before(:each) do
-			@options = {:operation => "ADD", :time => 10.0, :input => "test/samples/test_srt.srt", :output => "test_output.srt"}
-		end
+	before(:each) do
+		@options = {:operation => "ADD", :time => 10.0, :input => "test/samples/test_srt.srt", :output => "test_output.srt"}
+	end
 
-		after(:each) do
-			@options = nil
-		end
+	after(:each) do
+		@options = nil
+	end
 
-		describe "convert_time" do
+	describe "convert_time" do
+		
+		it "must add 2.5 seconds to a given time" do
 			
-			it "must add 2.5 seconds to a given time" do
-				
-				given_time = "00:00:10,000"
+			given_time = "00:00:10,000"
 
-				@options[:operation] = "ADD"
-				@options[:time] = 2.5
-				sub = SrtShifter::Subtitle.new(@options)
-				
-				sub.convert_time(given_time).must_equal "00:00:12,500"
-
-			end
-
-			it "must subtract 5.3 seconds to a given time" do
-
-				given_time = "00:00:10,000"
-
-				@options[:operation] = "SUB"
-				@options[:time] = 5.3
-				sub = SrtShifter::Subtitle.new(@options)
-
-				sub.convert_time(given_time).must_equal "00:00:04,700"
-
-			end
-
-			it "must accept time in '%H:%M:%S.%L' format in ADD operation" do
-				given_time = "00:00:10,000"
-				@options[:operation] = "ADD"
-				@options[:time] = "01:20:03.999"
-
-				sub = SrtShifter::Subtitle.new(@options)
-				sub.convert_time(given_time).must_equal "01:20:13,999"
-			end
-
-      it "must accept time in '+%H:%M:%S.%L' format in ADD operation" do
-				given_time = "00:00:10,000"
-				@options[:time] = "+01:20:03.999"
-
-				sub = SrtShifter::Subtitle.new(@options)
-				sub.convert_time(given_time).must_equal "01:20:13,999"
-			end
-
-			it "must accept time in '%H:%M:%S.%L' format in SUB operation" do
-				given_time = "01:20:03,999"
-				@options[:operation] = "SUB"
-				@options[:time] = "01:20:01.999"
-
-				sub = SrtShifter::Subtitle.new(@options)
-				sub.convert_time(given_time).must_equal "00:00:02,000"
-			end
-
-      it "must accept time in '-%H:%M:%S.%L' format in SUB operation" do
-        given_time = "01:20:03,999"
-        @options[:operation] = "SUB"
-        @options[:time] = "-01:20:01.999"
-
-        sub = SrtShifter::Subtitle.new(@options)
-        sub.convert_time(given_time).must_equal "00:00:02,000"
-      end
+			@options[:operation] = "ADD"
+			@options[:time] = 2.5
+			sub = SrtShifter::Subtitle.new(@options)
+			
+			sub.convert_time(given_time).must_equal "00:00:12,500"
 
 		end
 
-		describe "shift" do
-			
-			it "must generate a new srt file" do
-				sub = SrtShifter::Subtitle.new(@options)
-				sub.shift
-				File.exist?(@options[:output]).must_equal true
-			end
+		it "must subtract 5.3 seconds to a given time" do
 
+			given_time = "00:00:10,000"
+
+			@options[:operation] = "SUB"
+			@options[:time] = 5.3
+			sub = SrtShifter::Subtitle.new(@options)
+
+			sub.convert_time(given_time).must_equal "00:00:04,700"
+
+		end
+
+		it "must accept time in '%H:%M:%S.%L' format in ADD operation" do
+			given_time = "00:00:10,000"
+			@options[:operation] = "ADD"
+			@options[:time] = "01:20:03.999"
+
+			sub = SrtShifter::Subtitle.new(@options)
+			sub.convert_time(given_time).must_equal "01:20:13,999"
+		end
+
+    it "must accept time in '+%H:%M:%S.%L' format in ADD operation" do
+			given_time = "00:00:10,000"
+			@options[:time] = "+01:20:03.999"
+
+			sub = SrtShifter::Subtitle.new(@options)
+			sub.convert_time(given_time).must_equal "01:20:13,999"
+		end
+
+		it "must accept time in '%H:%M:%S.%L' format in SUB operation" do
+			given_time = "01:20:03,999"
+			@options[:operation] = "SUB"
+			@options[:time] = "01:20:01.999"
+
+			sub = SrtShifter::Subtitle.new(@options)
+			sub.convert_time(given_time).must_equal "00:00:02,000"
+		end
+
+    it "must accept time in '-%H:%M:%S.%L' format in SUB operation" do
+      given_time = "01:20:03,999"
+      @options[:operation] = "SUB"
+      @options[:time] = "-01:20:01.999"
+
+      sub = SrtShifter::Subtitle.new(@options)
+      sub.convert_time(given_time).must_equal "00:00:02,000"
+    end
+
+	end
+
+	describe "shift" do
+		
+		it "must generate a new srt file" do
+			sub = SrtShifter::Subtitle.new(@options)
+			sub.shift
+			File.exist?(@options[:output]).must_equal true
 		end
 
 	end
